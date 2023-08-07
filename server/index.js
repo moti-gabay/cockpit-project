b8d8a76a3ac9973d81f25ef5182922bbe858ad37
Moti Gabay <129662657+moti-gabay@users.noreply.github.com>
2023-08-07 04:05:44 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ import readline from 'node:readline/promises';
 import { Server } from 'socket.io';
 import http from 'node:http';
 
-const server = http.createServer();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+const server = http.createServer();
 const io = new Server(server, {
   cors: {
     origin: true,
@@ -13,24 +17,41 @@ const io = new Server(server, {
 
 server.listen(3001);
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-while (true) {
-  const altitude = await rl.question('ENTER ALTITUDE between 0 to 3000: '),
-    HIS = await rl.question('ENTER HIS between 0 to 360: '),
-    ADI = await rl.question('ENTER ADI  100 / -100 / 0: '),
-    areYouSure = await rl.question('Are you sure? (Y/n) ');
-
-  if (areYouSure !== 'n') {
-    io.emit('send_data', {
-      altitude,
-      HIS,
-      ADI
-    });
-    console.log('Message has been sent');
-  }
+async function askAltitude() {
+  return rl.question('ENTER ALTITUDE between 0 to 3000: ');
 }
 
+async function askHIS() {
+  return rl.question('ENTER HIS between 0 to 360: ');
+}
+
+async function askADI() {
+  return rl.question('ENTER ADI 100 / -100 / 0: ');
+}
+
+async function askConfirmation() {
+  return rl.question('sure? (1/0) ');
+}
+
+async function main() {
+  try {
+    const Altitude = await askAltitude();
+    const HIS = await askHIS();
+    const ADI = await askADI();
+    const sure = await askConfirmation();
+
+    if (sure === '1') {
+      io.emit('send_data', {
+        Altitude,
+        HIS,
+        ADI
+      });
+      console.log(' your Message sent');
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+  } finally {
+    rl.close();
+  }
+}
+main();
